5da4822685808cdc5b4a99f6591f73d4c7bb4297
rupeshcodess <153836164+rupeshcodess@users.noreply.github.com>
2024-02-23 21:48:26 +0530

    Add files via upload

--- ContinueBreak.java
+++ ContinueBreak.java
@@ -1,18 +1,20 @@
 class ContinueBreak
 {
-	public static void main(String args[])
-	{
-		LOOP1	:	for(int i=1;i<100;i++)
-		{
-			System.out.println(" ");
-			if(i>=10)break;
-			for(int j=1;j<100;j++)
-			{
-				System.out.println("*");
-				if(j==1)
-					continue LOOP1;
-			}
-		}
-		System.out.println("Termination by BREAK");
-	}
+public static void main(String args[])
+
+{
+LOOP1: for (int 1 1:1 < 100; i++)
+{
+
+System.out.println("");
+if (110) break:
+for (int j = 1; j < 100: j++)
+{
+
+System.out.print("*");
+if (ji)
+continue LOOP1:
+}
+}
+System.out.println("Termination by BREAK"):
 }
